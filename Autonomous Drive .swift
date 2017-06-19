0f74551e811a5101e3fc9d9f1db3cdb99f00f836
Sarp Gökdağ <sarpgo@icloud.com>
2017-06-20 00:46:08 +0300

    Update Autonomous Drive .swift

--- Autonomous Drive .swift
+++ Autonomous Drive .swift
@@ -1,14 +1,17 @@
+
 import Foundation
 
-var labyrinth = [[0,0,0,1],
-                 [0,1,1,1],
-                 [0,1,0,0],
-                 [0,1,1,1]]
+// 0 = road
+// 1 = Barrier
+var labyrinth = [[1,0,0,0],
+                 [0,0,1,1],
+                 [0,1,1,0],
+                 [0,0,0,0]]
 print("Here is Our Labyrinth = " , labyrinth)
-var been = [[1,1,1,1],
-            [1,1,1,1],
-            [1,1,1,1],
-            [1,1,1,1]]
+var been = [[0,0,0,0],
+            [0,0,0,0],
+            [0,0,0,0],
+            [0,0,0,0]]
 ////Matrix System starts with 0,1,2,3
 var x=3
 var y=3
@@ -24,7 +27,7 @@ for i in 0...10{
     if (y<3)
         //if y<3 continue from this block
     {
-        if (labyrinth[y+1][x]==1 && been[y+1][x]==1)
+        if (labyrinth[y+1][x]==0 && been[y+1][x]==0)
         {
             y=y+1
             print("Coordinate : \(x),\(y)")
@@ -36,7 +39,7 @@ for i in 0...10{
     if (y>0)
         //if y>0 continue from this block
     {
-        if (labyrinth[y-1][x]==1 && been[y-1][x]==1)
+        if (labyrinth[y-1][x]==0 && been[y-1][x]==0)
         {
             y=y-1
             print("Coordinate : \(x),\(y)")
@@ -48,7 +51,7 @@ for i in 0...10{
     if (x<3)
         //if x<3 continue from this block
     {
-        if (labyrinth[y][x+1]==1 && been[y][x+1]==1)
+        if (labyrinth[y][x+1]==0 && been[y][x+1]==0)
         {
             x=x+1
             print("Coordinate : \(x),\(y)")
@@ -60,7 +63,7 @@ for i in 0...10{
     if (x>0)
         //if x>0 continue from this block
     {
-        if (labyrinth[y][x-1]==1 && been[y][x-1]==1)
+        if (labyrinth[y][x-1]==0 && been[y][x-1]==0)
         {
             x=x-1
             print("Coordinate : \(x),\(y)")
@@ -74,4 +77,7 @@ for i in 0...10{
 print("Hello Again Passenger , We Have Arrived At Your Destination ! ~ Artificial Intelligence")
 
 
+
+//SARP GÖKDAĞ
+//23.01.2017
 
